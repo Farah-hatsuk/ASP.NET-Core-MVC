5e2a019adc8860ac49dd9d6907126a78cc878cb5
Farah-hatsuk <farah.hatsuk@gmail.com>
2025-02-16 23:29:56 +0300

    task1

---
+++ WebApplication1/WebApplication1/Controllers/User.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class User : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Rigester()
+        {
+            return View();
+        }
+
+        public IActionResult Profile()
+        {
+            return View();
+        }
+    }
+}
